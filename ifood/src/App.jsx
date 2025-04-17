1fe5351f410b0f0de068267e6157cc6104358624
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-04-17 08:41:27 -0300

    hhhh

--- ifood/src/App.jsx
+++ ifood/src/App.jsx
@@ -6,10 +6,7 @@ function App() {
 
   return (
     <>
-      <NavBar/>
-      <h1>iFoodzão</h1>
-      <p>O melhor aplicativo de entrega do Brasil</p>
-      <p>Baixe já o seu!</p>
+     
     </>
   )
 }
