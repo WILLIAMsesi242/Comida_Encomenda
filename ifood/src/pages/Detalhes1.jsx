f79d67e559bf70801186c5ce50e5990e116d2a5f
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-04-17 08:26:46 -0300

    hello

---
+++ ifood/src/pages/Detalhes1.jsx
@@ -0,0 +1,11 @@
+function Lanche() {
+
+    return (
+        <div>
+        <h1>Lanche</h1>
+        <p>Detalhes do lanche...</p>
+        </div>
+    );
+}
+
+export default Lanche
---
+++ ifood/src/pages/Detalhes3.jsx
@@ -0,0 +1,11 @@
+function Marmitex() {
+
+    return (
+        <div>
+        <h1>Marmitex</h1>
+        <p>Detalhes do produto Marmitex.</p>
+        </div>
+    );
+}
+
+export default Marmitex
